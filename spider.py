8bbe4096a4b4e96ce55f0692e443847faccc99d3
auggie yang <34760807+Auggie321@users.noreply.github.com>
2018-06-26 17:29:42 +0800

    spider_demo
    多线程抓取豆瓣10页的书籍demo
    书记网页地址：https://www.douban.com/doulist/1264675/
    注：测试一两次就行，多次抓取会被网站禁爬

---
+++ spider.py
@@ -0,0 +1,58 @@
+import re
+import io
+import requests
+import json
+from requests.exceptions import RequestException
+from multiprocessing import Pool
+
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+def parse_one_page(html):
+    pattern = re.compile('<div class="post">.*?src="(.*?)".*?blank">'
+                         +'(.*?)</a>.*?nums">(.*?)</span>.*?abstract">(.*?)<br />(.*?)<br />(.*?)</div>', re.S)
+    items = re.findall(pattern,html)
+#    print(items)
+    for item in items:
+        yield{
+            'img':item[0],
+            'name':item[1].strip(),
+            'score':item[2],
+            'writtor':item[3].strip(),
+            'publish orgnization':item[4].strip(),
+            'year':item[5].strip()
+        }
+
+def write_to_file(content):
+    with io.open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+        f.close
+
+def main(offset):
+    url = 'https://www.douban.com/doulist/1264675/?start='+ str(offset) +'&sort=seq&sub_type='
+    html = get_one_page(url)
+#    print(html)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+
+
+
+if __name__ == '__main__':
+#douban page = https://www.douban.com/doulist/1264675/?start=50&sort=seq&sub_type=
+#first page = i(i-1)*25 = 0
+#second page = i(i-1)*25 = 25
+#    non multiprocess get page1 to page10
+#    L = [(i-1)*25 for i in range(1,11)]
+#    for offset in L:
+#        main(offset)
+
+    pool = Pool()
+    pool.map(main,[(i-1)*25 for i in range(1,11)])
